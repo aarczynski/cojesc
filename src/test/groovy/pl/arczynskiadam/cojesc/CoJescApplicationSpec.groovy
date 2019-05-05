38010e412af7c10bdbdb5bc0b68ed0dbf377d17e
Adam Arczynski <adam.arczynski@ocado.com>
2019-05-05 14:33:27 +0200

    spock integration

---
+++ src/test/groovy/pl/arczynskiadam/cojesc/CoJescApplicationSpec.groovy
@@ -0,0 +1,18 @@
+package pl.arczynskiadam.cojesc
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import spock.lang.Specification
+
+@SpringBootTest(classes = CoJescApplication)
+class CoJescApplicationSpec extends Specification {
+
+    @Autowired
+    private ApplicationContext applicationContext
+
+    def "spring application context should run"() {
+        expect:
+        applicationContext
+    }
+}
