036d39f11954605637bd512a41f82c6f1ef2c7a8
Adam Arczynski <adam.arczynski@ocado.com>
2019-05-05 14:00:50 +0200

    app bootstrap
    fixed typo
    
    upgraded graddle wrapper to ver 5.4

---
+++ src/main/java/pl/arczynskiadam/cojesc/CoJescApplication.java
@@ -0,0 +1,13 @@
+package pl.arczynskiadam.cojesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoJescApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoJescApplication.class, args);
+    }
+
+}
