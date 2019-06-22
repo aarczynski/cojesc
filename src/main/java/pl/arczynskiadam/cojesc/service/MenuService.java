dd4837e576c25333398a5ea235b2b0328119bd42
Adam Arczynski <adam.arczynski@ocado.com>
2019-06-22 19:27:00 +0200

    added support for new type of restaurant - facebook feed

--- src/main/java/pl/arczynskiadam/cojesc/service/MenuService.java
+++ src/main/java/pl/arczynskiadam/cojesc/service/MenuService.java
@@ -2,6 +2,7 @@ package pl.arczynskiadam.cojesc.service;
 
 import org.springframework.stereotype.Service;
 import pl.arczynskiadam.cojesc.restaurant.FacebookAlbumRestaurant;
+import pl.arczynskiadam.cojesc.restaurant.FacebookFeedRestaurant;
 import pl.arczynskiadam.cojesc.restaurant.Restaurant;
 
 import java.util.Optional;
@@ -9,14 +10,19 @@ import java.util.Optional;
 @Service
 public class MenuService {
     FacebookAlbumMenuService facebookAlbumMenuService;
+    FacebookFeedMenuService facebookFeedMenuService;
 
-    public MenuService(FacebookAlbumMenuService facebookAlbumMenuService) {
+    public MenuService(FacebookAlbumMenuService facebookAlbumMenuService, FacebookFeedMenuService facebookFeedMenuService) {
         this.facebookAlbumMenuService = facebookAlbumMenuService;
+        this.facebookFeedMenuService = facebookFeedMenuService;
     }
 
     public Optional<String> findLunchMenuUrl(Restaurant restaurant) {
         if (restaurant instanceof FacebookAlbumRestaurant) {
             return facebookAlbumMenuService.getLunchMenuImageLink((FacebookAlbumRestaurant) restaurant);
+        }
+        if (restaurant instanceof FacebookFeedRestaurant) {
+            return facebookFeedMenuService.getLunchMenuLink((FacebookFeedRestaurant) restaurant);
         }
 
         return Optional.empty();
