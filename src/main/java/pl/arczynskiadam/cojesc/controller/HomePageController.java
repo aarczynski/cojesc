ae479fc757d7e29f99fecf41b7abb593dd344425
Adam Arczynski <adam.arczynski@ocado.com>
2019-06-23 22:08:44 +0200

    cache and logging

--- src/main/java/pl/arczynskiadam/cojesc/controller/HomePageController.java
+++ src/main/java/pl/arczynskiadam/cojesc/controller/HomePageController.java
@@ -1,13 +1,16 @@
 package pl.arczynskiadam.cojesc.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HomePageController {
 
     @GetMapping("/")
     public String home() {
+        log.info("Homepage visited");
         return "index";
     }
 }
