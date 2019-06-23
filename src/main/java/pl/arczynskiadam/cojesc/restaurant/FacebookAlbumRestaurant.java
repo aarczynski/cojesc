21c533e5ea1607f65030db8cfc9de6165821c737
Adam Arczynski <adam.arczynski@ocado.com>
2019-06-23 22:07:16 +0200

    lombok tostring fixes

--- src/main/java/pl/arczynskiadam/cojesc/restaurant/FacebookAlbumRestaurant.java
+++ src/main/java/pl/arczynskiadam/cojesc/restaurant/FacebookAlbumRestaurant.java
@@ -2,9 +2,11 @@ package pl.arczynskiadam.cojesc.restaurant;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class FacebookAlbumRestaurant extends Restaurant {
     private String facebookAlbumId;
 }
