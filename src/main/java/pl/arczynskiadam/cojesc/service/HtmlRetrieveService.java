297bd86b4c685d66225d687fbde85c9c0e5af2d4
Adam Arczynski <adam.arczynski@ocado.com>
2019-06-30 20:06:52 +0200

    added bez lukru restaurant support

--- src/main/java/pl/arczynskiadam/cojesc/service/HtmlRetrieveService.java
+++ src/main/java/pl/arczynskiadam/cojesc/service/HtmlRetrieveService.java
@@ -1,11 +1,16 @@
 package pl.arczynskiadam.cojesc.service;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Comment;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +23,8 @@ public class HtmlRetrieveService {
         Document doc = null;
         try {
             doc = Jsoup.connect(url.toString()).get();
+            uncomment(doc);
+            sanitize(doc);
         } catch (IOException e) {
             throw new IllegalArgumentException("Unable to http GET: " + url);
         }
@@ -26,7 +33,7 @@ public class HtmlRetrieveService {
                 .body()
                 .getElementsByClass(cssClass)
                 .stream()
-                .map(e -> e.toString())
+                .map(Node::toString)
                 .collect(toList());
     }
 
@@ -42,5 +49,36 @@ public class HtmlRetrieveService {
                 .body()
                 .getElementById(htmlId)
                 .toString());
+    }
+
+    private Document sanitize(Document doc) {
+        doc.select("script").remove();
+        doc.getAllElements().removeAttr("href");
+        return doc;
+    }
+
+    private Document uncomment(Document doc) {
+        List<Comment> comments = findAllComments(doc);
+        for (Comment comment : comments) {
+            String data = comment.getData();
+            comment.after(data);
+            comment.remove();
+        }
+        return doc;
+    }
+
+    private List<Comment> findAllComments(Document doc) {
+        List<Comment> comments = new ArrayList<>();
+        for (Element element : doc.getAllElements()) {
+            for (Node n : element.childNodes()) {
+                if (n.nodeName().equals("#comment")) {
+                    var comment = ((Comment) n).getData().replaceAll("\\s", "");
+                    if (!comment.contains("[if") && !comment.contains("[endif")) {
+                        comments.add((Comment) n);
+                    }
+                }
+            }
+        }
+        return Collections.unmodifiableList(comments);
     }
 }
